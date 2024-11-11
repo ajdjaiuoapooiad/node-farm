c56db6e3b104cfa06c923eaac6685cda9cd97732
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-11 19:49:07 +0900

    15ch ok

--- index.js
+++ index.js
@@ -8,14 +8,40 @@ const http = require('http');
 //Server
 
 
-const tempOverview = fs.readFileSync(`${__dirname}/templates/orverview.html`,'utf-8');
+
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/overview.html`,'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`,'utf-8');
 
+
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+
+
+
 const server = http.createServer((req,res) => {
-    console.log(req.url);
-    res.end('Hello server');
+    
+    const pathname = req.url;
+
+
+    // Overview page
+    if (pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, {
+        'Content-type': 'text/html'
+        });
+
+        res.end(tempOverview);
+
+        // Product page
+    } 
+
 });
+
+
+
 
 server.listen(8000,'127.0.0.1',() => {
     console.log('Listening to request on port 8000');
