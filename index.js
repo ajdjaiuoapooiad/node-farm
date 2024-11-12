258f19ad28d28acf1d4eb29244efec365e4fcfcb
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-12 13:48:18 +0900

    ok

--- index.js
+++ index.js
@@ -6,7 +6,21 @@ const http = require('http');
 // console.log(textIn);
 
 //Server
+const replaceTemplate = (temp,product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
+    output = output.replace(/{%IMAGE%}/g,product.iamge)
+    output = output.replace(/{%PRICE%}/g,product.price)
+    output = output.replace(/{%FROM%}/g,product.from)
+    output = output.replace(/{%NUTRIENTS%}/g,product.nutrients)
+    output = output.replace(/{%QUANTITY%}/g,product.quantity)
+    output = output.replace(/{%DESCRIPTION%}/g,product.description)
+    output = output.replace(/{%ID%}/g,product.id)
 
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic');
+    return output;
+
+
+}
 
 
 
@@ -27,16 +41,43 @@ const server = http.createServer((req,res) => {
     const pathname = req.url;
 
 
-    // Overview page
-    if (pathname === '/' || pathname === '/overview') {
-        res.writeHead(200, {
+// Overview page
+if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {
+    'Content-type': 'text/html'
+    });
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el));
+    console.log(cardsHtml);
+    
+
+    res.end(tempOverview);
+
+    // Product page
+} 
+  // Product page
+else if (pathname === '/product') {
+    res.writeHead(200, {
         'Content-type': 'text/html'
-        });
+    });
+   
 
-        res.end(tempOverview);
+    // API
+} else if (pathname === '/api') {
+    res.writeHead(200, {
+        'Content-type': 'application/json'
+    });
 
-        // Product page
-    } 
+    // Not found
+} else {
+    res.writeHead(404, {
+        'Content-type': 'text/html',
+        'my-own-header': 'hello-world'
+    });
+    res.end('<h1>Page not found!</h1>');
+}
+
+
 
 });
 
