69e51b85bf3eaeb26f3bf22c8006976673dc7b3b
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-12 14:34:49 +0900

    ok

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
+
 
 
 // const textIn = fs.readFileSync('./txt/input.txt','utf-8');
@@ -47,48 +49,47 @@ const dataObj = JSON.parse(data);
 
 
 
-const server = http.createServer((req,res) => {
-    
-    const pathname = req.url;
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
 
 
-// Overview page
-if (pathname === '/' || pathname === '/overview') {
+  // Overview page
+  if (pathname === '/' || pathname === '/overview') {
+      res.writeHead(200, {
+      'Content-type': 'text/html'
+      });
+
+      const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+      const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+      res.end(output);
+      
+  } 
+    // Product page
+  else if (pathname === '/product') {
     res.writeHead(200, {
-    'Content-type': 'text/html'
-    });
-
-    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
-    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
-    res.end(output);
-    
-} 
-  // Product page
-else if (pathname === '/product') {
-    res.writeHead(200, {
-        'Content-type': 'text/html'
+      'Content-type': 'text/html'
     });
     const product = dataObj[query.id];
     const output = replaceTemplate(tempProduct, product);
     res.end(output);
-   
+    
 
-    // API
-} else if (pathname === '/api') {
-    res.writeHead(200, {
-      'Content-type': 'application/json'
-    });
-    res.end(data);
-
-    // Not found
-} else {
-    res.writeHead(404, {
-        'Content-type': 'text/html',
-        'my-own-header': 'hello-world'
+      // API
+  } else if (pathname === '/api') {
+      res.writeHead(200, {
+        'Content-type': 'application/json'
       });
-      res.end('<h1>Page not found!</h1>');
+      res.end(data);
 
-}
+      // Not found
+  } else {
+      res.writeHead(404, {
+          'Content-type': 'text/html',
+          'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');
+
+  }
 
 
 
